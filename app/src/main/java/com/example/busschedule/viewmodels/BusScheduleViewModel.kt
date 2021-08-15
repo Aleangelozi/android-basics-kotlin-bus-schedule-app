c91b06b507eb64999d5e6b7ff78da92ec5d82d1e
aleangelozi <aleangelozi@gmail.com>
2021-08-15 17:21:09 -0300

    Kotlin update

--- app/src/main/java/com/example/busschedule/viewmodels/BusScheduleViewModel.kt
+++ app/src/main/java/com/example/busschedule/viewmodels/BusScheduleViewModel.kt
@@ -2,14 +2,14 @@ package com.example.busschedule.viewmodels
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.busschedule.database.schedule.Schedule
 import com.example.busschedule.database.schedule.ScheduleDao
-import java.lang.IllegalArgumentException
 
 class BusScheduleViewModel(private val scheduleDao: ScheduleDao): ViewModel() {
 
     fun fullSchedule(): List<Schedule> = scheduleDao.getAll()
 
-    fun scheduelForStopName(name: String): List<Schedule> = scheduleDao.getByStoName(name)
+    fun scheduelForStopName(name: String): List<Schedule> = scheduleDao.getByStopName(name)
 }
 
 class BusScheduleViewModelFactory(
